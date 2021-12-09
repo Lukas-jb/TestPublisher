8ecfa5a31bdb671cb4932c1c790f5aa1de7af493
Lukas-jb <92315994+Lukas-jb@users.noreply.github.com>
2021-12-09 16:21:33 -0500

    Unico Comit

---
+++ testpublisher/testpublisher/src/main/java/co/com/sofka/testpublisher/TestpublisherApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.testpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestpublisherApplication.class, args);
+	}
+
+}
---
+++ testpublisher/testpublisher/src/test/java/co/com/sofka/testpublisher/TestpublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.testpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
